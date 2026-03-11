0225f38b98b89e2f902072b2a813f40f599fd29f
Selva03-t <selvaganapathi03t@gmail.com>
2026-03-11 13:26:37 +0530

    added root route

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin:sam0604@cluster0.jh6p9wa.mongodb.net/contactform?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('✅ MongoDB Connected Successfully');
     } catch (err) {
         console.error('❌ MongoDB Connection Failed:', err.message);
